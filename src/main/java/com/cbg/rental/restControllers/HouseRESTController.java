accf5b5fc1b3ef51534f5e412d91935efc0882ca
CBG003 <cherifabenghorbel@gmail.com>
2024-11-05 19:13:24 +0100

    complete section2

--- src/main/java/com/cbg/rental/restControllers/HouseRESTController.java
+++ src/main/java/com/cbg/rental/restControllers/HouseRESTController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,33 +19,34 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 @RequestMapping("/api")
 @CrossOrigin
-public class HomeRESTController {
+public class HouseRESTController {
 	
 	@Autowired
 	HouseService houseService;
 	
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(path="all" ,method = RequestMethod.GET)
 	public List<House> getAllHouses() {
 		return houseService.getAllHouses();
 	}
 	
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@RequestMapping(value = "/getbyid/{id}", method = RequestMethod.GET)
 	public House getHouseByIds(@PathVariable("id") Long id) {
 		return houseService.getHouse(id);
 	}
 	
-	@RequestMapping(method = RequestMethod.POST)
+	@RequestMapping(value ="/addhouse",method = RequestMethod.POST)
+	//@PostMapping("/addhouse")
 	public House createHouse(@RequestBody House house) {
 		return houseService.saveHouse(house);
 	}
 	
-	@RequestMapping(method = RequestMethod.PUT)
+	@RequestMapping(value ="/updatehouse",method = RequestMethod.PUT)
 	public House updateHouse(@RequestBody House house) {
 		return houseService.updateHouse(house);
 	}
 	
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	@RequestMapping(value = "/delhouse/{id}", method = RequestMethod.DELETE)
 	public void deleteHouse(@PathVariable("id") Long id) {
 		 houseService.deleteHouseById(id);
 	}
